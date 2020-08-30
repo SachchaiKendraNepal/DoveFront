9e42156c89c34d17ce9be467c21ec8d1ccbdfd8e
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-08-30 22:47:09 +0545

    Added dove app layouts

---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="home-layout" class="d-flex flex-column">
+    <home-header/>
+    <div class="container">
+      <router-view/>
+    </div>
+    <home-footer/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SacchaiHomeLayout",
+  components: {
+    HomeHeader: () => import("@/views/home_layout/NavBar"),
+    HomeFooter: () => import("@/views/home_layout/Footer")
+  },
+}
+</script>
+
+<style lang="sass">
+#home-layout
+  height: 100%
+</style>
