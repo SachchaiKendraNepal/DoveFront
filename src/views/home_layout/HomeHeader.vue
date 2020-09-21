1815abcafb56dbb548992475b34f88ddd95cfedc
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-09-21 22:21:43 +0545

    Removed container from home header

--- src/views/home_layout/HomeHeader.vue
+++ src/views/home_layout/HomeHeader.vue
@@ -16,47 +16,15 @@
 			<v-icon>keyboard_arrow_up</v-icon>
 		</v-btn>
 		<home-tab />
-		<div id="sacchai-home-container">
-			<v-row justify="center">
-				<v-col
-					class="mb-0 pb-0"
-					v-show="$route.name === 'SACCHAI NEPAL'"
-					sm="3"
-					md="3"
-					lg="4"
-					xl="4"
-				>
-					<div class="float-lg-right float-md-right">
-						<user-clip />
-					</div>
-				</v-col>
-				<v-col class="mb-0 pb-0" sm="6" md="6" lg="4" xl="4">
-					<router-view />
-				</v-col>
-				<v-col
-					class="mb-0 pb-0"
-					v-show="$route.name === 'SACCHAI NEPAL'"
-					sm="3"
-					md="3"
-					lg="4"
-					xl="4"
-				>
-					<div class="float-lg-left float-md-left">
-						<user-clip />
-					</div>
-				</v-col>
-			</v-row>
-		</div>
+		<router-view />
 	</v-card>
 </template>
-
 <script>
 export default {
 	name: "TopNavTabComponent",
 	components: {
 		HomeTab: () => import("@/views/home_layout/Tab"),
-		ToolBar: () => import("@/views/home_layout/ToolBar"),
-		UserClip: () => import("@/views/home_layout/UserClip")
+		ToolBar: () => import("@/views/home_layout/ToolBar")
 	},
 	data() {
 		return {
@@ -74,6 +42,6 @@ export default {
 </script>
 
 <style lang="sass">
-#sacchai-home-container
-	background-color: #fbf2e5e8
+#home-header
+	background-color: rgba(236, 230, 221, 0.91)
 </style>
