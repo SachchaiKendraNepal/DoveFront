73633510caa8aa0623fdf94eb968bf3d39d3ab17
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-09-27 22:51:54 +0545

    Added detail view components

---
+++ src/components/DetailViewExplorer.vue
@@ -0,0 +1,84 @@
+<template>
+	<v-card
+		id="explore-card"
+		class="overflow-hidden"
+	>
+		<v-app-bar
+			dense
+			absolute
+			dark
+			color="#1365a6"
+			elevate-on-scroll
+			scroll-target="#scrolling-techniques-7"
+		>
+			<v-icon
+				size="30"
+				class="mr-2"
+			>
+				{{ toolbarIcon }}
+			</v-icon>
+			<v-toolbar-title id="explore-toolbar-title">
+				{{ toolbarTitle }}
+			</v-toolbar-title>
+			<v-spacer />
+			<v-btn icon>
+				<v-icon color="green lighten-4">
+					mdi-content-save-edit
+				</v-icon>
+			</v-btn>
+			<v-btn icon>
+				<v-icon color="red lighten-4">
+					mdi-delete
+				</v-icon>
+			</v-btn>
+		</v-app-bar>
+		<v-sheet
+			id="detail-sheet"
+			class="overflow-y-auto"
+			max-height="700"
+		>
+			<v-tabs
+				vertical
+			>
+				<v-tab
+					v-for="item in tabItems"
+					:id="item.id"
+					:key="item.index"
+					class="justify-start full-width"
+				>
+					<v-icon left>
+						{{ item.icon }}
+					</v-icon>
+					<span v-show="$vuetify.breakpoint.smAndUp">{{ item.title }}</span>
+				</v-tab>
+				<slot name="tabContent" />
+			</v-tabs>
+		</v-sheet>
+	</v-card>
+</template>
+<script>
+export default {
+	name: "DetailViewExplorerComponent",
+	props: {
+		toolbarTitle: {
+			type: String,
+			required: true
+		},
+		toolbarIcon: {
+			type: String,
+			required: true
+		},
+		tabItems: {
+			type: Array,
+			required: true
+		}
+	}
+}
+</script>
+<style lang="sass">
+#detail-sheet
+	margin-top: 48px
+#explore-toolbar-title
+	font-family: Roboto, sans-serif
+	font-weight: 300
+</style>
---
+++ src/components/DetailViewHeader.vue
@@ -0,0 +1,39 @@
+<template>
+	<v-row>
+		<v-col
+			cols="12"
+			xl="9"
+			lg="9"
+			md="9"
+			sm="9"
+			class="pl-10"
+		>
+			<slot name="topContent" />
+			<slot name="chips" />
+		</v-col>
+		<v-col
+			cols="12"
+			xl="3"
+			lg="3"
+			md="3"
+			sm="3"
+			class="text-center"
+		>
+			<slot name="image" />
+		</v-col>
+	</v-row>
+</template>
+<script>
+export default {
+	name: "DetailViewHeaderComponent",
+	props: {
+		item: {
+			type: Object,
+			required: true
+		}
+	},
+	data: () => ({
+
+	})
+}
+</script>
