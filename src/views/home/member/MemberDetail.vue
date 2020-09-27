675f855195ebb568a154f9c1ccef9a1c93a52905
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-09-27 22:53:04 +0545

    Used detail components

--- src/views/home/member/MemberDetail.vue
+++ src/views/home/member/MemberDetail.vue
@@ -4,15 +4,8 @@
 		max-width="900"
 		outlined
 	>
-		<v-row class="">
-			<v-col
-				cols="12"
-				xl="9"
-				lg="9"
-				md="9"
-				sm="9"
-				class="pl-10"
-			>
+		<detail-view-header>
+			<template #topContent>
 				<div class="overline">
 					FOLLOWER MEMBER
 				</div>
@@ -20,6 +13,8 @@
 					{{ follower.f_name }} {{ follower.l_name }}
 				</p>
 				<p>{{ follower.bio }}</p>
+			</template>
+			<template #chips>
 				<div>
 					<v-chip
 						class="mr-2 mb-3"
@@ -74,15 +69,8 @@
 						</v-icon>
 					</v-chip>
 				</div>
-			</v-col>
-			<v-col
-				cols="12"
-				xl="3"
-				lg="3"
-				md="3"
-				sm="3"
-				class="text-center"
-			>
+			</template>
+			<template #image>
 				<v-avatar
 					:size="
 						$vuetify.breakpoint.sm
@@ -95,207 +83,167 @@
 						:src="follower.image"
 					/>
 				</v-avatar>
-			</v-col>
-		</v-row>
-		<v-divider />
-
-		<v-tabs vertical>
-			<v-tab
-				v-for="item in profileTabItems"
-				:id="item.id"
-				:key="item.index"
-				class="text-start"
-			>
-				<v-icon left>
-					{{ item.icon }}
-				</v-icon>
-				<span v-show="$vuetify.breakpoint.smAndUp">{{ item.title }}</span>
-			</v-tab>
-
-			<v-tab-item>
-				<v-card
-					id="personal-info-tab-content"
-					flat
-				>
-					<v-card-text class="mt-0 pt-0">
-						<v-list class="mt-0 pt-0">
-							<v-list-item @click="1">
-								<v-icon class="mr-2">
-									mdi-account-circle
-								</v-icon>
-								<p class="mb-0 pb-0">
-									<b>Full Name:</b> {{ follower.f_name }} {{ follower.l_name }}
-								</p>
-							</v-list-item>
-							<v-list-item
-								v-for="(item, index) in personalInfoItems"
-								:key="index"
-								@click="1"
-							>
-								<v-icon class="mr-2">
-									{{ item.icon }}
-								</v-icon>
-								<p class="mb-0 pb-0">
-									<b>{{ item.displayFieldName }} :</b>
-									{{ follower[item.field] }}
-								</p>
-							</v-list-item>
-						</v-list>
-					</v-card-text>
-				</v-card>
-			</v-tab-item>
-			<v-tab-item>
-				<v-card
-					id="branch-info-tab-content"
-					flat
-				>
-					<v-card-text class="mt-0 pt-0">
-						<v-row>
-							<v-col
+			</template>
+		</detail-view-header>
+		<detail-view-explorer
+			id="explore-card"
+			toolbar-title="Explore Sachchai Follower"
+			toolbar-icon="mdi-account-circle"
+			:tab-items="profileTabItems"
+		>
+			<template #tabContent>
+				<v-tab-item>
+					<v-card
+						id="personal-info-tab-content"
+						flat
+					>
+						<v-card-text class="mt-0 pa-0">
+							<v-list class="mt-0 pt-0 alice-blue">
+								<v-list-item @click="1">
+									<v-icon class="mr-2">
+										mdi-account-circle
+									</v-icon>
+									<p class="mb-0 pb-0">
+										<b>Full Name:</b> {{ follower.f_name }} {{ follower.l_name }}
+									</p>
+								</v-list-item>
+								<v-list-item
+									v-for="(item, index) in personalInfoItems"
+									:key="index"
+									@click="1"
+								>
+									<v-icon class="mr-2">
+										{{ item.icon }}
+									</v-icon>
+									<p class="mb-0 pb-0">
+										<b>{{ item.displayFieldName }} :</b>
+										{{ follower[item.field] }}
+									</p>
+								</v-list-item>
+							</v-list>
+						</v-card-text>
+					</v-card>
+				</v-tab-item>
+				<v-tab-item>
+					<v-card
+						id="branch-info-tab-content"
+						flat
+					>
+						<v-card-text class="mt-0 pa-0">
+							<v-list
 								v-for="item in memberInfoItems1"
 								:key="item.index"
-								cols="8"
-								class="my-0 py-0"
+								class="ma-0 pa-0 alice-blue"
 							>
-								<v-list class="ma-0 pa-0">
-									<v-list-item @click="1">
-										<v-icon class="mr-2">
-											{{ item.icon }}
-										</v-icon>
-										<p class="ma-0 pa-0">
-											<b>{{ item.displayFieldName }} :</b>
-											{{ follower[item.field] }}
-										</p>
-										<v-spacer />
-										<span v-if="item.field === 'position'">
-											<v-avatar v-for="i in 2"
-												:key="i"
-												class="elevation-3"
-												size="20"
-												color="grey darken-2 ml-1"
-											>
-												<v-icon dark
-													small
-												>mdi-star</v-icon>
-											</v-avatar>
-										</span>
-									</v-list-item>
-								</v-list>
-							</v-col>
-							<v-col
-								cols="8"
-								class="my-0 py-0"
-							>
-								<v-list class="ma-0 pa-0">
-									<v-list-item @click="1">
-										<v-checkbox
-											v-model="follower.is_approved"
-											color="indigo"
-											label="Approved Status"
-										/>
-										<v-spacer />
-										<span>
-											<v-icon>mdi-marker-check</v-icon>
-										</span>
-									</v-list-item>
-								</v-list>
-							</v-col>
-							<v-col
+								<v-list-item @click="1">
+									<v-icon class="mr-2">
+										{{ item.icon }}
+									</v-icon>
+									<p class="ma-0 pa-0">
+										<b>{{ item.displayFieldName }} :</b>
+										{{ follower[item.field] }}
+									</p>
+									<v-spacer />
+									<span v-if="item.field === 'position'">
+										<v-avatar v-for="i in 2"
+											:key="i"
+											class="elevation-3"
+											size="20"
+											color="grey darken-2 ml-1"
+										>
+											<v-icon dark
+												small
+											>mdi-star</v-icon>
+										</v-avatar>
+									</span>
+								</v-list-item>
+							</v-list>
+							<v-list class="ma-0 pa-0 alice-blue">
+								<v-list-item @click="1">
+									<v-checkbox
+										v-model="follower.is_approved"
+										color="indigo"
+										label="Approved Status"
+									/>
+									<v-spacer />
+									<span>
+										<v-icon>mdi-marker-check</v-icon>
+									</span>
+								</v-list-item>
+							</v-list>
+							<v-list
 								v-for="item in memberInfoItems2"
 								:key="item.index"
-								cols="8"
-								class="my-0 py-0"
+								class="ma-0 pa-0 alice-blue"
 							>
-								<v-list class="ma-0 pa-0">
-									<v-list-item @click="1">
-										<v-icon class="mr-2">
-											{{ item.icon }}
-										</v-icon>
-										<p class="ma-0 pa-0">
-											<b>{{ item.displayFieldName }} :</b>
-											{{ follower[item.field] }}
-										</p>
-									</v-list-item>
-								</v-list>
-							</v-col>
-						</v-row>
-					</v-card-text>
-				</v-card>
-			</v-tab-item>
-			<v-tab-item>
-				<v-card
-					id="permission-info-tab-content"
-					flat
-				>
-					<v-card-text class="mt-0 pt-0">
-						<v-row>
-							<v-col
+								<v-list-item @click="1">
+									<v-icon class="mr-2">
+										{{ item.icon }}
+									</v-icon>
+									<p class="ma-0 pa-0">
+										<b>{{ item.displayFieldName }} :</b>
+										{{ follower[item.field] }}
+									</p>
+								</v-list-item>
+							</v-list>
+						</v-card-text>
+					</v-card>
+				</v-tab-item>
+				<v-tab-item>
+					<v-card
+						id="permission-info-tab-content"
+						flat
+					>
+						<v-card-text class="mt-0 pa-0">
+							<v-list
 								v-for="(item, index) in permissionInfoItems"
 								:key="index"
-								cols="10"
-								class="ma-0 pa-0"
+								class="ma-0 pa-0 alice-blue"
 							>
-								<v-list class="ma-0 pa-0">
-									<v-list-item @click="1">
-										<v-checkbox
-											v-model="follower[item.field]"
-											color="indigo"
-											:label="item.displayFieldName"
-										/>
-										<v-spacer />
-										<span>
-											<v-icon>{{ item.icon }}</v-icon>
-										</span>
-									</v-list-item>
-								</v-list>
-							</v-col>
-							<v-col cols="12">
-								<permission-table />
+								<v-list-item @click="1">
+									<v-checkbox
+										v-model="follower[item.field]"
+										dense
+										:label="item.displayFieldName"
+									/>
+									<v-spacer />
+									<span>
+										<v-icon>{{ item.icon }}</v-icon>
+									</span>
+								</v-list-item>
+							</v-list>
+							<div class="mt-2" />
+							<permission-table />
+						</v-card-text>
+					</v-card>
+				</v-tab-item>
+				<v-tab-item>
+					<v-card
+						id="posts-info-tab-content"
+						flat
+					>
+						<v-row
+							justify="center"
+							align="center"
+							class="ma-0 pa-0"
+						>
+							<v-col class="pa-4 pt-0 pt-4 alice-blue">
+								<v-card
+									flat
+									class="ma-0 pa-0 center-align alice-blue"
+									max-width="500"
+								>
+									<FollowerArticle />
+									<FollowerMultimedia />
+									<FollowerArticle />
+									<FollowerMultimedia />
+								</v-card>
 							</v-col>
 						</v-row>
-					</v-card-text>
-				</v-card>
-			</v-tab-item>
-			<v-tab-item>
-				<v-card
-					id="posts-info-tab-content"
-					flat
-				>
-					<v-row>
-						<v-col class="ma-4 mt-1">
-							<FollowerArticle />
-							<FollowerMultimedia />
-							<FollowerArticle />
-							<FollowerMultimedia />
-						</v-col>
-					</v-row>
-				</v-card>
-			</v-tab-item>
-		</v-tabs>
-
-		<v-card-actions>
-			<v-spacer />
-			<v-btn
-				color="red lighten-5"
-				class="red--text"
-				depressed
-			>
-				<v-icon small>
-					mdi-delete
-				</v-icon>
-				Delete
-			</v-btn>
-			<v-btn
-				color="blue lighten-5"
-				class="blue--text"
-				depressed
-			>
-				<v-icon small>
-					mdi-content-save-edit
-				</v-icon>
-				Save
-			</v-btn>
-		</v-card-actions>
+					</v-card>
+				</v-tab-item>
+			</template>
+		</detail-view-explorer>
 	</v-card>
 </template>
 
@@ -305,7 +253,9 @@ export default {
 	components: {
 		PermissionTable: () => import("@/components/PermissionTable"),
 		FollowerArticle: () => import("@/views/home/article/Article"),
-		FollowerMultimedia: () => import("@/views/home/multimedia/Multimedia")
+		FollowerMultimedia: () => import("@/views/home/multimedia/Multimedia"),
+		DetailViewHeader: () => import("@/components/DetailViewHeader"),
+		DetailViewExplorer: () => import("@/components/DetailViewExplorer")
 	},
 	data: () => ({
 		now: null,
@@ -341,6 +291,7 @@ export default {
 			{icon: "mdi-account-tie", field: "is_staff", displayFieldName: "Can View Admin Site"},
 			{icon: "mdi-account-cowboy-hat", field: "is_superuser", displayFieldName: "Super User"}
 		],
+		collapseOnScroll: true,
 		follower: {
 			id: 1,
 			username: "kiran589",
@@ -373,6 +324,8 @@ export default {
 }
 </script>
 <style lang="sass" scoped>
+.alice-blue
+	background-color: aliceblue
 .headline
 	padding-top: 10px
 	font-size: 36px !important
@@ -384,9 +337,11 @@ export default {
 	border-radius: 5px
 	color: #f8f8d9
 	font-weight: bold
-.text-start
-	align-self: start
-.personal-info-tab-content-card
-
-	margin-right: 10px
+.center-align
+	@media only screen and (min-width: 720px)
+		margin-left: 40px !important
+	@media only screen and (min-width: 800px)
+		margin-left: 80px !important
+#explore-card
+	border-radius: 0
 </style>
