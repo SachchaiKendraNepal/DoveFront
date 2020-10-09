d0b472e246556542a4a9d605e7c2c3cb3619c60b
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-10-10 01:03:18 +0545

    Integrated multiple upload in startpost

---
+++ src/components/DerpressedButtonWithToolTip.vue
@@ -0,0 +1,50 @@
+<template>
+	<v-tooltip bottom>
+		<template #activator="{ on, attrs }">
+			<v-btn
+				class="my-2"
+				depressed
+				v-bind="attrs"
+				v-on="on"
+			>
+				<v-icon
+					:size="size"
+					:color="color"
+				>
+					{{ icon }}
+				</v-icon>
+				<span class="btn-text">{{ text }}</span>
+			</v-btn>
+		</template>
+		<span>{{ tooltip }}</span>
+	</v-tooltip>
+</template>
+<script>
+export default {
+	name: "DepressedButtonComponent",
+	props: {
+		icon: {
+			type: String,
+			required: true
+		},
+		text: {
+			type: String,
+			required: true
+		},
+		size: {
+			type: String,
+			required: true
+		},
+		color: {
+			type: String,
+			required: true
+		},
+		tooltip: {
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
+<style lang="sass" scoped>
+</style>
