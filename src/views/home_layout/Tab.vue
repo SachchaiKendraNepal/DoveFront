a943f48d614480cd2d8ada88000952402525eeed
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-10-11 23:51:25 +0545

    Added event in home tabs

--- src/views/home_layout/Tab.vue
+++ src/views/home_layout/Tab.vue
@@ -2,6 +2,7 @@
 	<v-tabs
 		id="sacchai-home-tab"
 		v-model="tab"
+		class="elevation-2"
 		centered
 		show-arrows
 		center-active
@@ -17,7 +18,9 @@
 			:to="item.to"
 		>
 			{{ item.name }}
-			<v-icon>{{ item.icon }}</v-icon>
+			<v-icon size="25">
+				{{ item.icon }}
+			</v-icon>
 		</v-tab>
 	</v-tabs>
 </template>
@@ -32,13 +35,19 @@ export default {
 				{ name: "Branches", icon: "mdi-home-group", to: "/home/branch" },
 				{ name: "Followers", icon: "mdi-account-group", to: "/home/follower" },
 				{ name: "Articles", icon: "mdi-post", to: "/home/article" },
-				{ name: "Multimedia", icon: "mdi-video-vintage", to: "/home/multimedia" }
+				{ name: "Multimedia", icon: "mdi-video-vintage", to: "/home/multimedia" },
+				{ name: "Events", icon: "mdi-calendar-multiple", to: "/home/event" }
 			]
 		}
 	}
 }
 </script>
 <style lang="sass">
+#sacchai-home-tab
+	position: -webkit-sticky
+	position: sticky
+	top: 70px
+	z-index: 100
 a[role="tab"]
 	font-size: 12px
 	font-weight: bold
