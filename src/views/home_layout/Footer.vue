b480e791769e642fc8099230df4971542b048dca
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-11-02 00:42:08 +0545

    Redesigned dove-front footer

--- src/views/home_layout/Footer.vue
+++ src/views/home_layout/Footer.vue
@@ -4,35 +4,132 @@
 		elevation="3"
 	>
 		<v-card
-			color="rgb(255 242 202)"
+			class="flex"
+			color="#d2d6db"
 			flat
 			tile
-			class="text-center"
 		>
-			<v-card-text>
-				<v-btn
-					v-for="(item, index) in icons"
-					:key="index"
-					class="mx-4"
-					icon
+			<v-row
+				class="ma-0 pa-0"
+				align-center="center"
+				justify="center"
+			>
+				<v-col
+					cols="12"
+					xl="3"
+					lg="3"
+					md="3"
+					sm="3"
+					class="pl-xl-16 pl-lg-16 pl-md-12 pl-sm-6"
 				>
-					<v-icon
-						size="24px"
-						:color="item.color"
+					<div class="d-flex justify-start align-center">
+						<v-avatar
+							tile
+							size="50"
+						>
+							<v-img src="@/assets/dove-solid.svg" />
+						</v-avatar>
+						<span id="org-title">SACHCHAI KENDRA NEPAL</span>
+					</div>
+					<div id="about-kendra">
+						<h4 class="flex text-start">
+							A very constructive and inclusive social network. Open source and radically transparent.
+						</h4>
+						<v-card-actions class="d-flex justify-center ma-0 pa-0">
+							<v-btn
+								v-for="(item, index) in socialMediaButtonIcons"
+								:key="index"
+								class="mx-2"
+								icon
+							>
+								<v-icon
+									size="24px"
+									:color="item.color"
+								>
+									{{ item.icon }}
+								</v-icon>
+							</v-btn>
+						</v-card-actions>
+						<h5 class="flex text-start">
+							SACHCHAI community copyright 2019 - 2020
+						</h5>
+						<h6 id="built-info"
+							class="flex text-start"
+						>
+							Built on Vuetify -- the open source software that powers SACHCHAI KENDRA NEPAL `DoveFront` and other inclusive communities.
+						</h6>
+						<h6 class="flex text-start">
+							Made with love and NodeJs.
+						</h6>
+					</div>
+				</v-col>
+				<v-col>
+					<v-list dense
+						max-width="120"
+						color="transparent"
 					>
-						{{ item.icon }}
-					</v-icon>
-				</v-btn>
-			</v-card-text>
-
-			<v-card-text class="pt-0">
-				Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo
-				interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed
-				massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi
-				vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui
-				sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque
-				penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-			</v-card-text>
+						<v-list-item-group
+							color="black"
+						>
+							<v-list-item
+								v-for="(item, i) in firstList"
+								:key="i"
+							>
+								<v-list-item-content>
+									<v-list-item-title>{{ item.text }}</v-list-item-title>
+								</v-list-item-content>
+							</v-list-item>
+						</v-list-item-group>
+					</v-list>
+				</v-col>
+				<v-col>
+					<v-list dense
+						max-width="130"
+						color="transparent"
+					>
+						<v-list-item-group
+							color="black"
+						>
+							<v-list-item
+								v-for="(item, i) in secondList"
+								:key="i"
+							>
+								<v-list-item-content>
+									<v-list-item-title>{{ item.text }}</v-list-item-title>
+								</v-list-item-content>
+							</v-list-item>
+						</v-list-item-group>
+					</v-list>
+				</v-col>
+				<v-col>
+					<v-list dense
+						max-width="120"
+						color="transparent"
+					>
+						<v-list-item-group
+							color="black"
+						>
+							<v-list-item
+								v-for="(item, i) in thirdList"
+								:key="i"
+							>
+								<v-list-item-content>
+									<v-list-item-title>{{ item.text }}</v-list-item-title>
+								</v-list-item-content>
+							</v-list-item>
+						</v-list-item-group>
+					</v-list>
+				</v-col>
+				<v-col
+					cols="12"
+					xl="3"
+					lg="3"
+					md="4"
+					sm="6"
+				>
+					<facebook-widget />
+				</v-col>
+			</v-row>
 
 			<v-divider />
 
@@ -46,18 +143,63 @@
 <script>
 export default {
 	name: "HomeFooter",
+	components: {
+		FacebookWidget: () => import("@/components/showcase/Facebook")
+	},
 	data: () => ({
-		icons: [
+		logo: require("@/assets/sacchai_logo.jpg"),
+		socialMediaButtonIcons: [
 			{color: "#3a80ec", icon: "mdi-facebook"},
 			{color: "#f71701", icon: "mdi-youtube"},
 			{color: "#bb2d71", icon: "mdi-instagram"},
 			{color: "rgb(62 161 242)", icon: "mdi-twitter"}
+		],
+		firstList: [
+			{text: "Home"},
+			{text: "Reading List"},
+			{text: "Bookmarks"},
+			{text: "Events"},
+			{text: "Videos"},
+			{text: "Tags"},
+		],
+		secondList: [
+			{text: "Code of Conduct"},
+			{text: "FAQ"},
+			{text: "Our Artifacts"},
+			{text: "Sponsors"},
+			{text: "About"},
+			{text: "Privacy Policy"},
+		],
+		thirdList: [
+			{text: "Terms of use"},
+			{text: "Contact Us"},
+			{text: "Feedbacks"},
+			{text: "Write a post"},
 		]
 	})
 }
 </script>
 
-<style lang="sass">
-.gradient
-	background: rgb(93, 19, 91) !important
+<style lang="sass" scoped>
+#about-kendra
+	h4
+		font-size: 14px !important
+		font-weight: 450 !important
+		padding: 4px 0
+		color: #545454
+	h5
+		color: #545454
+		font-weight: normal
+		padding-top: 5px
+		padding-bottom: 10px
+	h6
+		color: #545454
+		font-weight: normal
+	#built-info
+		padding-bottom: 10px
+#org-title
+	font-size: 22px
+	padding-left: 10px
+	font-family: "Crimson Text", sans-serif
+	font-weight: 600
 </style>
