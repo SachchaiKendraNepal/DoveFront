039daa06be4de5da36e20d6f28f646b8e8865e6e
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-11-01 19:29:38 +0545

    Added active state in link items

--- src/components/showcase/AppBarLinkBtn.vue
+++ src/components/showcase/AppBarLinkBtn.vue
@@ -1,20 +1,27 @@
 <template>
-	<v-btn depressed
-		color="transparent"
+	<v-item
+		v-slot="{ active, toggle }"
 	>
-		<v-icon
-			v-if="$vuetify.breakpoint.smAndDown"
-			size="22"
-			color="rgb(20 2 52)"
+		<v-btn
+			class="ma-1 link-button"
+			depressed
+			:color="active ? 'primary' : 'transparent'"
+			@click="toggle"
 		>
-			{{ icon }}
-		</v-icon>
-		<span
-			v-if="$vuetify.breakpoint.mdAndUp"
-			class="pt-1 pl-1"
-			style="color:#FF9800"
-		>{{ text }}</span>
-	</v-btn>
+			<v-icon
+				v-if="$vuetify.breakpoint.smAndDown"
+				size="22"
+				:color="active ? 'teal darken-3' : 'black'"
+			>
+				{{ icon }}
+			</v-icon>
+			<span
+				v-if="$vuetify.breakpoint.mdAndUp"
+				class="pt-1 pl-1"
+				:style="active ? 'color: teal; border-bottom: 2px dotted #00808066; border-width: 10px;': 'color: black'"
+			>{{ text }}</span>
+		</v-btn>
+	</v-item>
 </template>
 <script>
 export default {
@@ -31,3 +38,13 @@ export default {
 	}
 }
 </script>
+<style lang="sass" scoped>
+.link-button
+	background-color: transparent !important
+.link-text
+	color: black !important
+.text-active
+		color: white !important
+.active
+	background-color: #426acb !important
+</style>
