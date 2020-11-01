bdd39c795059237da00559fac81f81a534479490
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-11-01 19:42:35 +0545

    Used dynamic classes for active texts

--- src/components/showcase/AppBarLinkBtn.vue
+++ src/components/showcase/AppBarLinkBtn.vue
@@ -5,20 +5,19 @@
 		<v-btn
 			class="ma-1 link-button"
 			depressed
-			:color="active ? 'primary' : 'transparent'"
+			color="transparent"
 			@click="toggle"
 		>
 			<v-icon
-				v-if="$vuetify.breakpoint.smAndDown"
 				size="22"
-				:color="active ? 'teal darken-3' : 'black'"
+				:color="active ? 'teal darken-2' : 'black'"
 			>
 				{{ icon }}
 			</v-icon>
 			<span
 				v-if="$vuetify.breakpoint.mdAndUp"
-				class="pt-1 pl-1"
-				:style="active ? 'color: teal; border-bottom: 2px dotted #00808066; border-width: 10px;': 'color: black'"
+				class="pt-1 pl-1 link-text"
+				:class="active ? 'active-link-text': 'non-active-link-text'"
 			>{{ text }}</span>
 		</v-btn>
 	</v-item>
@@ -39,12 +38,12 @@ export default {
 }
 </script>
 <style lang="sass" scoped>
-.link-button
-	background-color: transparent !important
 .link-text
-	color: black !important
-.text-active
-		color: white !important
-.active
-	background-color: #426acb !important
+	font-size: 14px
+.active-link-text
+	color: green
+	border-bottom: 10px dotted forestgreen
+.non-active-link-text
+	color: black
+
 </style>
