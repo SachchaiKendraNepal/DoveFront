55b785a08c3ce73098da50681c688b69bb4c86fc
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-11-18 03:02:24 +0545

    Moved home sidenav quick links to separate component

--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -108,37 +108,7 @@
 				floating
 			>
 				<user-clip />
-				<v-list>
-					<v-list-group
-						v-for="item in items"
-						:key="item.title"
-						v-model="item.active"
-						active-class="darkblue--text"
-						no-action
-					>
-						<template #prependIcon>
-							<v-icon :color="item.color">
-								{{ item.action }}
-							</v-icon>
-						</template>
-						<template #activator>
-							<v-list-item-content>
-								<v-list-item-title v-text="item.title" />
-							</v-list-item-content>
-						</template>
-
-						<v-list-item
-							v-for="child in item.items"
-							:key="child.title"
-							:to="child.to"
-						>
-							<v-list-item-icon><v-icon>{{ child.icon }}</v-icon></v-list-item-icon>
-							<v-list-item-content>
-								<v-list-item-title v-text="child.title" />
-							</v-list-item-content>
-						</v-list-item>
-					</v-list-group>
-				</v-list>
+				<quick-links />
 			</v-navigation-drawer>
 			<v-main>
 				<v-container
@@ -169,6 +139,7 @@ export default {
 		ProfileDropdown: () => import("@/views/home_layout/ProfileDropdown"),
 		UserClip: () => import("@/views/home_layout/UserClip"),
 		HomeFooter: () => import("@/views/home_layout/Footer"),
+		QuickLinks: () => import("@/views/home_layout/QuickLinks"),
 		ScrollUp: () => import("@/components/ScrollTop")
 	},
 	data: () => ({
@@ -180,99 +151,7 @@ export default {
 			{icon: "mdi-video-vintage", text: "Multimedia", to: "/home/multimedia"},
 			{icon: "mdi-calendar-clock", text: "Events", to: "/home/event"},
 		],
-		homeDrawer: true,
-		items: [
-			{
-				color: "grey darken-3",
-				action: "mdi-star-circle",
-				items: [
-					{ title: "My Profile", icon: "mdi-account-circle", to: "/home/profile" },
-					{ title: "My Posts", icon: "mdi-post" },
-					{ title: "My Medias", icon: "mdi-video-vintage" },
-					{ title: "My Bookmarks", icon: "mdi-bookmark" },
-				],
-				title: "My Links",
-			},
-			{
-				color: "indigo",
-				action: "mdi-city",
-				items: [
-					{ title: "Province", icon: "mdi-office-building-marker-outline" },
-					{ title: "District", icon: "mdi-map-marker-multiple-outline" },
-					{ title: "VDC", icon: "mdi-home-map-marker" },
-					{ title: "Municipality", icon: "mdi-google-maps" },
-				],
-				title: "Branch",
-			},
-			{
-				color: "red darken-1",
-				action: "mdi-calendar-clock",
-				active: false,
-				items: [
-					{ title: "All Events", icon: "mdi-calendar-multiple" },
-					{ title: "Satsang", icon: "mdi-heart" },
-					{ title: "Kendra Events", icon: "mdi-calendar-star" },
-					{ title: "Branch Events", icon: "mdi-calendar-text" },
-					{ title: "General Meets", icon: "mdi-account-supervisor-circle" },
-				],
-				title: "Events",
-			},
-			{
-				color: "orange darken-2",
-				action: "mdi-post",
-				items: [
-					{ title: "Photos", icon: "mdi-image" }
-				],
-				title: "Articles",
-			},
-			{
-				color: "teal darken-2",
-				action: "mdi-video-vintage",
-				items: [
-					{ title: "Sounds", icon: "mdi-music-box" },
-					{ title: "Photos", icon: "mdi-image" },
-					{ title: "Videos", icon: "mdi-video-box" },
-				],
-				title: "Multimedia",
-			},
-			{
-				color: "blue darken-4",
-				action: "mdi-earth",
-				items: [
-					{ title: "Motivation", icon: "mdi-run" },
-					{ title: "Health", icon: "mdi-bottle-tonic-plus" },
-					{ title: "Peace", icon: "mdi-peace" },
-					{ title: "Dance", icon: "mdi-dance-ballroom" },
-				],
-				title: "Our Services",
-			},
-			{
-				color: "pink darken-1",
-				action: "mdi-google-maps",
-				items: [
-					{ title: "Branches", icon: "mdi-city" },
-					{ title: "Kendra Branch", icon: "mdi-map-marker-star" },
-				],
-				title: "Maps",
-			},
-			{
-				color: "purple darken-4",
-				action: "mdi-office-building",
-				items: [
-					{ title: "About Us", icon: "mdi-information" },
-					{ title: "Contact Us", icon: "mdi-phone" },
-					{ title: "Kendra Message", icon: "mdi-message" },
-					{ title: "Write Feedback", icon: "mdi-comment-quote" },
-				],
-				title: "Office",
-			},
-			{
-				color: "green darken-3",
-				action: "mdi-tag",
-				items: [{ title: "Our Partners", icon: "mdi-handshake" }],
-				title: "Promotions",
-			},
-		]
+		homeDrawer: true
 	}),
 	methods: {
 		routeToShowcase() {
