3fd84de11478440bec34fafc6b504731ee7c5001
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-11-25 22:13:48 +0545

    Added branch api methods

--- src/store/modules/branch.js
+++ src/store/modules/branch.js
@@ -1,4 +1,7 @@
 import $api from "@/handler/axios"
+import urls from "@/urls.json"
+
+const branchUrl = urls.branch
 
 export const SET_BRANCHES = "SET_BRANCHES"
 export const SET_BRANCH = "SET_BRANCH"
@@ -24,9 +27,21 @@ const getters = {
 }
 
 const actions = {
-	async getAllBranches({commit}) {
-		const response = await $api.get("/branch/")
+	async getAll({commit}) {
+		const response = await $api.get(branchUrl)
 		commit("SET_BRANCHES", response)
+	},
+
+	async create({commit}, body) {
+		await $api.post(branchUrl, body)
+	},
+
+	async update({commit}, payload) {
+		await $api.put(branchUrl + payload.id + "/", payload.body)
+	},
+
+	async delete({commit}, payload) {
+		await $api.delete(branchUrl + payload.id + "/")
 	}
 }
 
