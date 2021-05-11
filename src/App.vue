da7f52388e659bc1a7321085d6372f8565f56692
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-05-11 08:02:09 +0545

    Globalized video container css

--- src/App.vue
+++ src/App.vue
@@ -12,3 +12,27 @@ export default {
 	})
 }
 </script>
+<style lang="scss">
+.quick-sand-font {
+	font-family: 'Quicksand', sans-serif;
+}
+.video-container {
+	overflow: hidden;
+	position: relative;
+	width:100%;
+}
+
+.video-container::after {
+	padding-top: 56.25%;
+	display: block;
+	content: '';
+}
+
+.video-container iframe {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+}
+</style>
