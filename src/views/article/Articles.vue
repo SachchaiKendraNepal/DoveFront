2d53304af0f9c9c241d67f7785e6530261c2c3f9
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-05-18 23:08:31 +0545

    Removed outer v-row wrapper from datatable

--- src/views/article/Articles.vue
+++ src/views/article/Articles.vue
@@ -1,90 +1,84 @@
 <template>
-	<v-row
-		justify="center"
-		align="center"
-		class="ma-0 pa-1 my-12"
+	<v-data-table
+		:loading="loadingArticles"
+		loading-text="Fetching articles from server"
+		:headers="articleListHeaders"
+		:items="articles"
+		item-key="name"
+		class="elevation-3 mx-12 my-6"
 	>
-		<v-data-table
-			:loading="loadingArticles"
-			loading-text="Fetching articles from server"
-			:headers="articleListHeaders"
-			:items="articles"
-			item-key="name"
-			class="elevation-1"
-		>
-			<template #top>
-				<v-toolbar flat
-					color="grey lighten-2"
-				>
-					<v-app-bar-nav-icon>
-						<v-avatar class="nav-avatar elevation-4 ma-1"
-							size="38"
-						>
-							<v-icon class="mx-0">
-								mdi-bird
-							</v-icon>
-						</v-avatar>
-					</v-app-bar-nav-icon>
-					<v-toolbar-title class="text-uppercase table-title mx-0 px-0">
-						Sachchai Nepal Articles
-					</v-toolbar-title>
-					<v-spacer />
-					<v-icon>mdi-star</v-icon>
-				</v-toolbar>
-			</template>
-			<!-- eslint-disable-next-line vue/valid-v-slot-->
-			<template #item.approval_status="{ item }">
-				<v-switch v-model="item.is_approved"
-					color="grey darken-2"
-					disabled
-				/>
-			</template>
-			<!-- eslint-disable-next-line vue/valid-v-slot-->
-			<template #item.actions="{ item }">
-				<v-icon
-					v-if="!item.is_approved"
-					small
-					color="green"
-					class="mr-2"
-					@click="updateArticleApproval(item)"
-				>
-					mdi-check
-				</v-icon>
-				<v-icon
-					v-if="item.is_approved"
-					small
-					color="red"
-					class="mr-2"
-					@click="updateArticleApproval(item)"
-				>
-					mdi-close
-				</v-icon>
-				<v-icon
-					color="blue"
-					small
-					class="mr-2"
-					@click="editArticle(item)"
-				>
-					mdi-pencil
-				</v-icon>
-				<v-icon
-					small
-					color="red"
-					@click="deleteArticle(item)"
-				>
-					mdi-delete
-				</v-icon>
-			</template>
-			<template #no-data>
-				<v-btn
-					color="primary"
-					@click="initialize"
-				>
-					Reset
-				</v-btn>
-			</template>
-		</v-data-table>
-	</v-row>
+		<template #top>
+			<v-toolbar flat
+				color="grey lighten-2"
+			>
+				<v-app-bar-nav-icon>
+					<v-avatar class="nav-avatar elevation-4 ma-1"
+						size="38"
+					>
+						<v-icon class="mx-0">
+							mdi-bird
+						</v-icon>
+					</v-avatar>
+				</v-app-bar-nav-icon>
+				<v-toolbar-title class="text-uppercase table-title mx-0 px-0">
+					Sachchai Nepal Articles
+				</v-toolbar-title>
+				<v-spacer />
+				<v-icon>mdi-star</v-icon>
+			</v-toolbar>
+		</template>
+		<!-- eslint-disable-next-line vue/valid-v-slot-->
+		<template #item.approval_status="{ item }">
+			<v-switch v-model="item.is_approved"
+				color="grey darken-2"
+				disabled
+			/>
+		</template>
+		<!-- eslint-disable-next-line vue/valid-v-slot-->
+		<template #item.actions="{ item }">
+			<v-icon
+				v-if="!item.is_approved"
+				small
+				color="green"
+				class="mr-2"
+				@click="updateArticleApproval(item)"
+			>
+				mdi-check
+			</v-icon>
+			<v-icon
+				v-if="item.is_approved"
+				small
+				color="red"
+				class="mr-2"
+				@click="updateArticleApproval(item)"
+			>
+				mdi-close
+			</v-icon>
+			<v-icon
+				color="blue"
+				small
+				class="mr-2"
+				@click="editArticle(item)"
+			>
+				mdi-pencil
+			</v-icon>
+			<v-icon
+				small
+				color="red"
+				@click="deleteArticle(item)"
+			>
+				mdi-delete
+			</v-icon>
+		</template>
+		<template #no-data>
+			<v-btn
+				color="primary"
+				@click="initialize"
+			>
+				Reset
+			</v-btn>
+		</template>
+	</v-data-table>
 </template>
 <script>
 import {mapGetters} from "vuex";
@@ -95,13 +89,13 @@ export default {
 		return {
 			loadingArticles: false,
 			articleListHeaders: [
+				{ text: "Actions", value: "actions", sortable: false },
 				{ text: "ID", value: "id" },
 				{ text: "Title", value: "title" },
 				{ text: "Author", value: "uploaded_by.username" },
 				{ text: "Status", value: "approval_status" },
 				{ text: "Approver", value: "approved_by.username" },
-				{ text: "Created At", value: "uploaded_at" },
-				{ text: "Actions", value: "actions", sortable: false },
+				{ text: "Created At", value: "uploaded_at" }
 			]
 		}
 	},
