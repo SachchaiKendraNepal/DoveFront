c981b7f4075e519f273e0e7e02900837ff6475c5
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-05-28 01:53:54 +0545

    Added youtube iframe component

---
+++ src/components/YoutubeIframe.vue
@@ -0,0 +1,32 @@
+<template>
+	<iframe v-if="videoUrl" :src="prepareEmbedUrl(videoUrl)"
+		frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+		allowfullscreen
+		height="100%"
+		width="100%"
+	/>
+</template>
+
+<script>
+export default {
+	name: "YoutubeIframe",
+	props: {
+		videoUrl: {
+			type: String,
+			required: true
+		}
+	},
+	methods: {
+		getId(videoUrl) {
+			return this.$helper.getVideoIdFromYoutubeURL(videoUrl)
+		},
+		prepareEmbedUrl(videoUrl) {
+			return `https://www.youtube.com/embed/${this.getId(videoUrl)}`
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
