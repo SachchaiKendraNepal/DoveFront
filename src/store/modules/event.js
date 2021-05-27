6c44ecc7f371ae277ce340d666612b2b032c560b
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-05-28 01:55:12 +0545

    Added event related store actions

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -7,6 +7,8 @@ const eventUrl = urls.event
 export const SET_EVENTS = "SET_EVENTS"
 export const SET_EVENT = "SET_EVENT"
 export const SET_EVENT_FORM_ERRORS = "SET_EVENT_FORM_ERRORS"
+export const SET_EVENT_STATISTICS = "SET_EVENT_STATISTICS"
+export const SET_EVENT_COMMENTS = "SET_EVENT_COMMENTS"
 
 const defaultFormErrors = {
 	title: null,
@@ -36,6 +38,8 @@ const state = {
 	event: {},
 	eventPhotos: {},
 	eventVideoUrls: {},
+	statistics: {},
+	comments: {},
 	formErrors: { ... defaultFormErrors }
 }
 
@@ -48,13 +52,21 @@ const mutations = {
 	},
 	[SET_EVENT_FORM_ERRORS](state, value) {
 		state.formErrors = value
+	},
+	[SET_EVENT_STATISTICS](state, value) {
+		state.statistics = value
+	},
+	[SET_EVENT_COMMENTS](state, value) {
+		state.comments = value
 	}
 }
 
 const getters = {
 	list: state => state.events.results,
 	detail: state => state.event,
-	formErrorsList: state => state.formErrors
+	formErrorsList: state => state.formErrors,
+	statisticsDetail: state=> state.statistics,
+	commentsList: state=> state.comments
 }
 
 const actions = {
@@ -119,6 +131,66 @@ const actions = {
 			await $api.post(util.format(eventUrl.toggleApproval, id))
 			return true
 		} catch {
+			return false
+		}
+	},
+
+	async fetchStatistics({commit}, {id: id}) {
+		try {
+			const response = await $api.get(util.format(eventUrl.interestStatistics, id))
+			commit("SET_EVENT_STATISTICS", response)
+			return true
+		} catch (e) {
+			return false
+		}
+	},
+
+	async toggleInterestedStatus({}, {id: id,}) {
+		try {
+			await $api.post(util.format(eventUrl.toggleInterestedStatus, id))
+			return true
+		} catch (e) {
+			return false
+		}
+	},
+	async toggleGoingStatus({}, {id: id}) {
+		try {
+			await $api.post(util.format(eventUrl.toggleGoingStatus, id))
+			return true
+		} catch (e) {
+			return false
+		}
+	},
+	async fetchCommentsFor({commit}, {id: id}) {
+		try {
+			const response = await $api.getWithPayload(eventUrl.commentList, {event: id})
+			commit("SET_EVENT_COMMENTS", response)
+			return true
+		} catch (e) {
+			return false
+		}
+	},
+	async addImageListFor({}, {id: id, body: body}) {
+		try {
+			await $api.post(util.format(eventUrl.addImageList, id), body)
+			return true
+		} catch (e) {
+			return false
+		}
+	},
+	async addVideoListFor({}, {id: id, body: body}) {
+		try {
+			await $api.post(util.format(eventUrl.addVideoList, id), body)
+			return true
+		} catch (e) {
+			return false
+		}
+	},
+	async addVideoUrlListFor({}, {id: id, body: body}) {
+		try {
+			await $api.post(util.format(eventUrl.addVideoUrlList, id), body)
+			return true
+		} catch (e) {
 			return false
 		}
 	}
