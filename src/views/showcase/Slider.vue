4c6fb37652f945cefc22207c2a65960ab83203f1
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-06-24 10:34:00 +0545

    Single image in showcase slider

--- src/views/showcase/Slider.vue
+++ src/views/showcase/Slider.vue
@@ -1,10 +1,8 @@
 <template>
 	<div class="showcase-img-wrapper">
-		<section v-for="(item, index) in items"
-			:key="index" class="panel"
-		>
+		<section>
 			<v-img class="showcase-img"
-				:src="item.src"
+				:src="require('@/assets/cosmos_flower.jpg')"
 				dark
 				gradient="to top right, rgb(0 0 0 / 0%), rgb(0 0 0 / 60%), rgb(0 0 0 / 20%)"
 				height="100vh"
@@ -27,78 +25,9 @@ export default {
 		SliderImageContent: () => import("@/views/showcase/SliderImageContent")
 	},
 	data: () => ({
-		loading: false,
-		items: [
-			{src: require("@/assets/cosmos_flower.jpg"), text: "Kiran Parajuli"},
-			{src: "https://wallpaperaccess.com/full/19849.jpg", text: "Beauty Wallpaper"},
-			{src: "https://cdn.wallpapersafari.com/10/51/h6L8oM.jpg", text: "Wallpaper Safari"},
-		],
+		loading: false
 	}),
-	mounted() {
-		let duration = 5,
-			sections = gsap.utils.toArray(".panel"),
-			sectionIncrement = duration / (sections.length - 1),
-			tl = gsap.timeline({
-				scrollTrigger: {
-					trigger: ".showcase-img-wrapper",
-					pin: true,
-					scrub: 1,
-					snap: 1 / (sections.length - 1),
-					start: "top top",
-					end: "+=5000",
-					endTrigger: ".showcase-img-wrapper"
-				}
-			});
-
-		tl.to(sections, {
-			xPercent: -100 * (sections.length - 1),
-			duration: duration,
-			ease: "none"
-		});
-
-		// everything below this is just for the fading/scaling up which is NOT scrubbed
-		// it's all dynamic, triggered when each section enters/leaves so that the fading/scaling occurs at a consistent rate no matter how fast you scroll!
-		sections.forEach((section, index) => {
-			let tween = gsap.from(section, {
-				opacity: 0,
-				scale: 0.6,
-				duration: 1,
-				force3D: true,
-				paused: true
-			});
-			this.addSectionCallbacks(tl, {
-				start: sectionIncrement * (index - 0.99),
-				end: sectionIncrement * (index + 0.99),
-				onEnter: () => tween.play(),
-				onLeave: () => tween.reverse(),
-				onEnterBack: () => tween.play(),
-				onLeaveBack: () => tween.reverse()
-			});
-			index || tween.progress(1); // the first tween should be at its end (already faded/scaled in)
-		});
-	},
-	methods: {
-		// helper function that lets us define a section in a timeline that spans between two times (start/end) and lets us add onEnter/onLeave/onEnterBack/onLeaveBack callbacks
-		addSectionCallbacks(timeline, {start, end, param, onEnter, onLeave, onEnterBack, onLeaveBack}) {
-			let trackDirection = animation => { // just adds a "direction" property to the animation that tracks the moment-by-moment playback direction (1 = forward, -1 = backward)
-					let onUpdate = animation.eventCallback("onUpdate"), // in case it already has an onUpdate
-						prevTime = animation.time();
-					animation.direction = animation.reversed() ? -1 : 1;
-					animation.eventCallback("onUpdate", () => {
-						let time = animation.time();
-						if (prevTime !== time) {
-							animation.direction = time < prevTime ? -1 : 1;
-							prevTime = time;
-						}
-						onUpdate && onUpdate.call(animation);
-					});
-				},
-				empty = v => v; // in case one of the callbacks isn't defined
-			timeline.direction || trackDirection(timeline); // make sure direction tracking is enabled on the timeline
-			start >= 0 && timeline.add(() => ((timeline.direction < 0 ? onLeaveBack : onEnter) || empty)(param), start);
-			end <= timeline.duration() && timeline.add(() => ((timeline.direction < 0 ? onEnterBack : onLeave) || empty)(param), end);
-		}
-	}
+	methods: {}
 
 }
 </script>
