6ebd83389db1b8368e59b842231681bf9c23d153
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-07-06 01:32:06 +0545

    Fixed facebook iframe with card wrapper

--- src/views/showcase/Facebook.vue
+++ src/views/showcase/Facebook.vue
@@ -3,20 +3,24 @@
 		<div class="sidebar-social-media-title">
 			<span><u>Follow Us On Facebook</u></span>
 		</div>
-		<!--		<div class="facebook-container">-->
-		<!--			<div class="fb-page"-->
-		<!--				data-href="https://www.facebook.com/Sachhaikendra-Nepal-2186630364921086" data-tabs="timeline"-->
-		<!--				data-height="350"-->
-		<!--				data-small-header="false" data-adapt-container-width="true"-->
-		<!--				data-hide-cover="false" data-show-facepile="true"-->
-		<!--			>-->
-		<!--				<blockquote cite="https://www.facebook.com/Sachhaikendra-Nepal-2186630364921086"-->
-		<!--					class="fb-xfbml-parse-ignore"-->
-		<!--				>-->
-		<!--					<a href="https://www.facebook.com/Sachhaikendra-Nepal-2186630364921086">Sachchai Kendra Nepal</a>-->
-		<!--				</blockquote>-->
-		<!--			</div>-->
-		<!--		</div>-->
+		<v-card :height="($vuetify.breakpoint.width < 900) ? 600 : 350"
+			class="overflow-hidden"
+		>
+			<div class="fb-page"
+				data-href="https://www.facebook.com/Sachhaikendra-Nepal-2186630364921086" data-tabs="timeline,messages"
+				:data-height="($vuetify.breakpoint.width < 900) ? 600 : 350"
+				data-width="500"
+				data-small-header="false" data-adapt-container-width="true"
+				data-hide-cover="false" data-show-facepile="true"
+				data-lazy="true"
+			>
+				<blockquote cite="https://www.facebook.com/Sachhaikendra-Nepal-2186630364921086"
+					class="fb-xfbml-parse-ignore"
+				>
+					<a href="https://www.facebook.com/Sachhaikendra-Nepal-2186630364921086">Sachhai Kendra Nepal</a>
+				</blockquote>
+			</div>
+		</v-card>
 	</div>
 </template>
 <script>
@@ -44,6 +48,7 @@ export default {
 	.facebook-container
 		margin-top: 5px
 		display: block
+		height: 30vh
 		@media only screen and (max-width: 600px)
 			padding: 0 20px
 		@media only screen and (max-width: 200px)
