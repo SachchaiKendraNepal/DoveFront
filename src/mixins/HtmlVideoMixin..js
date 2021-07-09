ed680d8c1aa2db61d762210791fe54cfc9ef5698
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-07-10 00:13:59 +0545

    Single source for video mixin

--- src/mixins/HtmlVideoMixin..js
+++ src/mixins/HtmlVideoMixin..js
@@ -1,6 +1,12 @@
 const HtmlVideoMixin = {
+	computed: {
+		player() {
+			return this.$refs.youtube.yt
+		}
+	},
 	methods: {
-		onPlay(e) {
+		async onPlay(e) {
+			await this.pauseAllYt()
 			document.querySelectorAll("video")
 				.forEach(video => {
 					if (video !== e.currentTarget) {
@@ -9,7 +15,34 @@ const HtmlVideoMixin = {
 						}
 					}
 				})
-		}
+		},
+		pauseAllPlayingHTMLVideos() {
+			document.querySelectorAll("video")
+				.forEach(video => {
+					if (!video.paused) {
+						video.pause()
+					}
+				})
+		},
+		async playing(e) {
+			const currentPlaying = e.getVideoUrl()
+			this.pauseAllPlayingHTMLVideos()
+			if (Array.isArray(this.$refs.yt)) {
+				await this.$refs.yt.forEach(async ref => {
+					const url = await ref.player.getVideoUrl()
+					if (url !== currentPlaying) {
+						await ref.player.pauseVideo()
+					}
+				})
+			}
+		},
+		async pauseAllYt() {
+			if (Array.isArray(this.$refs.yt)) {
+				await this.$refs.yt.forEach(async ref => {
+					await ref.player.pauseVideo()
+				})
+			}
+		},
 	},
 }
 
